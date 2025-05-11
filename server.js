9b92e21bc33128a138189552e3752ffc1154b0b7
ITPro-Hemedi <hemedicharles@gmail.com>
2025-05-11 11:19:42 +0200

    updated

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
 });
 app.get('/Esther', (req, res) => {
     res.send("Esther Grace");
-  });re
+  });
  
 app.listen(process.env.PORT || 3000, () => {
   console.log('Web Server is listening at port ' + (process.env.PORT || 3000));
